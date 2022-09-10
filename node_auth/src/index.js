0c24a5f0c6d46da6258663636ecb8a4d6d9713a0
miikaforma <miika.forma@codecontrol.fi>
2022-09-10 19:41:32 +0300

    Added Dockerfile for node_auth

--- node_auth/src/index.js
+++ node_auth/src/index.js
@@ -91,7 +91,7 @@ fastify.post('/wattivahti/token', async (request, reply) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 3000, host: '0.0.0.0' })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
